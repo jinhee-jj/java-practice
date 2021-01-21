72ef359545bdd5b8020160e5f650bc0e2aeb1f9d
Jun Jinhee <wjswlsgml93@gmail.com>
2021-01-21 12:01:44 +0900

    20200121 반복문 연습문제 추가

--- src/javapractice/forWhilePractice/ForWhilePractice.java
+++ src/javapractice/forWhilePractice/ForWhilePractice.java
@@ -148,4 +148,164 @@ public class ForWhilePractice {
 		
 		
 	}
+	
+//	2보다 큰 정수를 입력 받고, 입력 받은 정수가 소수이면 
+//	“소수다“, 소수가 아니면 “소수가 아니다.” 출력하고 프로그램을 종료하는 코드를 작성하세요.
+//	단, 입력 받은 정수가 2보다 작은 경우 “잘못
+//	입력하셨습니다. 다시 입력하세요.” 출력 후 다시 정수를 입력 받도록 하세요.
+//  ** 소수란? : 어떠한 정수를 1부터 자기 자신까지로 나누었을 때, 나누어 떨어지는 수가 1과 자기 자신 뿐인 수를 소수라고 한다.
+	public void practice06() {
+		
+		System.out.print("2보다 큰 정수를 입력하세요 : ");
+		int num = sc.nextInt();
+		int sum = 0;
+		for(int i = 1; i <= num; i++) {
+			
+			if(num % i == 0) {
+				sum += i;
+			}
+		}
+		
+		if(sum == num + 1) {
+			System.out.println("소수다");
+		} else {
+			System.out.println("소수가 아니다");
+		}
+	}
+	
+//	어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 
+//	바꾸는 암호화 방식을 시저 암호라고 합니다. A를 3만큼 밀면 D가 되고, 
+//	z를 1만큼 밀면 a가 됩니다. 공백문자는 변경되지 않습니다.
+//	문자열 1개를 입력 받고, 얼마나 문자를 밀지 알려주는 0보다 
+//	큰 정수 1개를 입력 받아 암호문을 작성하는 코드를 작성해보세요.
+//  예) “a B z”와 4를 입력 받았다면 “e F d” 출력
+//	“a B z”와 27을 입력 받았다면 “b C a” 출력
+	public void practice07() {
+		
+		
+		
+		int a = 'a';
+		int z = 'z';
+		
+		int bigA = 'A';
+		int bigz = 'Z';
+		
+		System.out.println(a);
+		System.out.println(z);
+		System.out.println(bigA);
+		System.out.println(bigz);
+		
+		
+		System.out.print("알파벳 세 개 각각 띄워서 입력 :  ");
+		String str = sc.nextLine();
+		
+		int one = str.charAt(0);
+		int two = str.charAt(2);
+		int three = str.charAt(4);
+		
+		
+		System.out.print("밀 갯수 입력 : ");
+		int num = sc.nextInt();
+		
+		char first = (char) (one + num);
+		char second = (char) (two + num);
+		char third = (char) (three + num);
+		
+		
+		while(first > 122 || second > 90 || third > 122) {
+			
+			if(first > 122) {
+				first = (char) (first - 122 + 96);
+			} else if(second > 90) {
+				second = (char) (second - 90 + 64);
+			} else if(third > 122) {
+				third = (char) (third - 122 + 96);
+			}
+		}
+		
+		System.out.println(first + " " + second + " " + third);
+		
+	}
+	
+//	받은 금액을 입력 받고, 상품 가격을 입력 받아 거스름돈을 
+//	계산하는 프로그램을 작성하세요.
+//	단, 거스름돈은 대한민국 화폐 단위 별 필요 수량이 출력 되도록 
+//	작성하고, 지폐와 동전을 구분하여 단위를 표기하세요.
+	 public void practice08() {
+		 
+		 System.out.print("받으신 금액을 입력하세요 : ");
+		 int price = sc.nextInt();
+		 System.out.print("상품 가격을 입력하세요 : ");
+		 int menuPrice = sc.nextInt();
+		 
+		 int originalChange = price - menuPrice;
+		 int change = price - menuPrice;
+		 int a = 50000;
+		 int b = 10000;
+		 int c = 5000;
+		 int d = 1000;
+		 int e = 500;
+		 int f = 100;
+		 int g = 50;
+		 int h = 10;
+		 
+		 
+		 
+		 System.out.println("============================");
+		 do {
+			 if(change >= a) {
+				 
+				 int num = change / a;
+				 change = change - num * a;
+				 
+				 System.out.println(a + "원권 지폐 " + num + "장");
+			 } else if(change >= b) {
+				 
+				 int num2 = change / b;
+				 change = change - num2 * b;
+				 System.out.println(b + "원권 지폐 " + num2 + "장");
+			 } else if(change >= c) {
+				 
+				 int num3 = change / c;
+				 change = change - num3 * c;
+				 System.out.println(c + "원권 지폐 " + num3 + "장");
+			 
+			 } else if(change >= d) {
+				 
+				 int num4 = change / d;
+				 change = change - num4 * d;
+				 System.out.println(d + "원권 지폐 " + num4 + "장");
+				 
+			 } else if(change >= e) {
+				 
+				 int num5 = change / e;
+				 change = change - num5 * e;
+				 System.out.println(e + "원권 동전 " + num5 + "개");
+			 
+			 } else if(change >= f) {
+				 
+				 int num6 = change / f;
+				 change = change - num6 * f;
+				 System.out.println(f + "원권 동전 " + num6 + "개");
+			 
+			 } else if(change >= g) {
+				 int num7 = change / g;
+				 change = change - num7 * g;
+				 System.out.println(g + "원권 동전 " + num7 + "개");
+			 
+			 } else if(change >= h) {
+				 
+				 int num8 = change / h;
+				 change = change - num8 * h;
+				 System.out.println(h + "원권 동전" + num8 + "개");
+			 }
+			 
+			 
+		 } while(change != 0);
+		 	 
+		 
+		 System.out.println("============================");
+		 System.out.println("거스름돈 : " + originalChange + "원");
+	 }
+	
 }
--- src/javapractice/forWhilePractice/TestMain.java
+++ src/javapractice/forWhilePractice/TestMain.java
@@ -3,9 +3,18 @@ package javapractice.forWhilePractice;
 public class TestMain {
 	public static void main(String[] args) {
 		ForWhilePractice p = new ForWhilePractice();
+		ForWhilePractice2 p2 = new ForWhilePractice2();
 		//p.practice01();
 		//p.practice03();
 		//p.practice04();
-		p.practice05();
+		//p.practice05();
+		//p.practice06();
+		//p.practice07();
+		//p.practice08();
+		
+		
+		//p2.printStart1();
+		//p2.printStar2();
+		p2.countInputCharacter();
 	}
 }
